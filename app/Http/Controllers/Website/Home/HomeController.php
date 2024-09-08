539cb29f7295403aa596c41f7826408a34b4f011
ktyudha <ktyudha@gmail.com>
2024-09-08 14:03:46 +0700

    update controller

--- app/Http/Controllers/Website/Home/HomeController.php
+++ app/Http/Controllers/Website/Home/HomeController.php
@@ -11,6 +11,7 @@ use App\Models\Management;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\GalleryResource;
+use App\Http\Resources\KatalogResource;
 use App\Http\Resources\CarouselResource;
 
 class HomeController extends Controller
@@ -23,10 +24,10 @@ class HomeController extends Controller
 
     public function index(Request $request)
     {
-        $data['sliders']           = CarouselResource::collection(Slider::all());
-        $data['galleries']         = GalleryResource::collection(Gallery::take('10')->get());
+        $data['sliders']           = CarouselResource::collection(Slider::take('4')->latest()->get());
+        $data['galleries']         = GalleryResource::collection(Gallery::take('10')->latest()->get());
         // $data['services']          = Service::Service()->take('3')->get();
-        $data['keunggulan']        = Service::Keunggulan()->take('10')->latest()->get();
+        $data['keunggulan']        = KatalogResource::collection(Service::Keunggulan()->take('10')->latest()->get());
         // $data['about_info']        = Management::About()->first();
         // $data['contact_info']      = Management::Contact()->first();
         // $data['social_info']      = Management::Social()->get();
